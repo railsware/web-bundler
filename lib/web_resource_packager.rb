b0697101489c464c4e7971cf90cb8558b7aa0821
gregolsen <anotheroneman@yahoo.com>
2010-10-11 14:10:15 +0300

    bugfix

--- lib/web_resource_packager.rb
+++ lib/web_resource_packager.rb
@@ -1,7 +1,6 @@
 $:.unshift File.dirname(__FILE__)
 require 'web_resource_packager/block_parser'
 require 'web_resource_packager/block_data'
-require 'web_resource_packager/resource_bundle_data'
 require 'web_resource_packager/file_packager'
 require 'web_resource_packager/settings'
 require 'web_resource_packager/image_to_css.rb'
