d22a2e4db5e2af27b893383500f89d9d36440576
gregolsen <anotheroneman@yahoo.com>
2010-11-12 00:13:56 +0200

    Version bump to 0.0.0

--- test_app/bundler_test/vendor/plugins/web_resource_bundler/lib/web_resource_bundler/filters.rb
+++
@@ -1,9 +0,0 @@
-require 'filters/base_filter'
-require 'filters/image_encode_filter'
-require 'filters/bundle_filter'
-require 'filters/cdn_filter'
-
-module WebResourceBundler
-  module Filters
-  end
-end
