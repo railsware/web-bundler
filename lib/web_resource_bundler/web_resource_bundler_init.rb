accd854f990912437801437d17009c8bdd0ae98b
gregolsen <anotheroneman@yahoo.com>
2010-12-18 20:06:53 +0200

    initializer is now up to date

--- lib/web_resource_bundler/web_resource_bundler_init.rb
+++ lib/web_resource_bundler/web_resource_bundler_init.rb
@@ -1,9 +1,3 @@
-require 'web_resource_bundler'
-require 'yaml'
-root_dir = Rails.root #or RAILS_ROOT if you are using older rails version than 3 
-environment = Rails.env
-settings = { }
-
-
-WebResourceBundler::Bundler.instance.set_settings(settings)
+WebResourceBundler::Bundler.instance.setup(Rails.root, Rails.env)
 ActionView::Base.send(:include, WebResourceBundler::RailsAppHelpers)
+
