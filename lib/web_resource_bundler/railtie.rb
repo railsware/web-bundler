d9f3be846aece161bfa73254778482ac82028c7f
gregolsen <anotheroneman@yahoo.com>
2011-03-19 23:37:55 +0200

    [#9023027] we should init bundler on App initialization

--- lib/web_resource_bundler/railtie.rb
+++ lib/web_resource_bundler/railtie.rb
@@ -1,6 +1,10 @@
 require 'rails'
 
-class WebResourceBundler::Railtie < Rails::Railtie
-  WebResourceBundler::Bundler.setup(Rails.root, Rails.env)
-  ActionView::Base.send(:include, WebResourceBundler::RailsAppHelpers)
+module WebResourceBundler
+  class Railtie < Rails::Railtie
+    initializer "web_resource_bundler_initializer.configure_rails_initialization" do
+      WebResourceBundler::Bundler.setup(Rails.root, Rails.env)
+      ActionView::Base.send(:include, WebResourceBundler::RailsAppHelpers)
+    end
+  end
 end
